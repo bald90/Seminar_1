5937fa143d21b1d0e29a2eccf16fc26f2e1d8d2e
Andrey <6198760@gmail.com>
2022-11-26 19:36:48 +0300

    Добавили решение 1,3 задачи

---
+++ Zadacha_0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear(); //команда для очистки терминала
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+//Console.Write(number);
+int sqr = number * number;
+//Console.WriteLine("Квадрат числа " + number + " равен " + sqr); взамен строки 7, но увеличивает стороку изза пробелов
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
